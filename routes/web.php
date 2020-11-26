c4a9002311147e2071a16150b79f685939e292ce
Cristian Etchebest <cristian.etchebest@meta.com.br>
2020-11-26 15:46:01 -0300

    Projeto final rodando

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+    // ROTAS GET
+	Route::get('/', function () {return view('welcome');});
+    Route::get('/produtos/pesquisar', 'ProdutoController@pesquisar');
+    Route::get('/produtos/inserir', 'ProdutoController@mostrar_inserir');
+    Route::get('/produtos/alterar/{id}', 'ProdutoController@mostrar_alterar');
+    Route::get('/produtos/excluir/{id}', 'ProdutoController@excluir');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+    // ROTAS POST
+    Route::post('/produtos/pesquisar', 'ProdutoController@pesquisar');
+    Route::post('/produtos/inserir', 'ProdutoController@inserir');
+    Route::post('/produtos/alterar', 'ProdutoController@alterar');
+
+
+
